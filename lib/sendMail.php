e69ba67136aee7d29568b46eef396c6701392a87
Michael Nugent <nugent.michael@gmail.com>
2015-02-07 16:27:05 -0800

    Updated Contact Information and General Fixes
    – Updated contact information to display new email address
    – Fixed portfolio content
    – Added new navigation menu link to display résumé
    – Updated contact form to send messages to new email address

--- lib/sendMail.php
+++ lib/sendMail.php
@@ -9,7 +9,7 @@
 		$message = trim($_POST['message']);
 	}
 		
-	$emailTo = 'nugent.michael@gmail.com';
+	$emailTo = 'michaelnugent@nugentic.com';
 	$subject = 'Contact Form Submission from '.$name;
 	$sendCopy = trim($_POST['sendCopy']);
 	$body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
